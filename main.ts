6d2558a3d3cac3ba7691a5217b2d9aca615a3127
theway <liiiv97@outlook.com>
2022-07-19 16:46:09 +0800

    first done.

--- main.ts
+++ main.ts
@@ -1,137 +1,94 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {Arr} from "tern";
+import {start} from "repl";
 
-// Remember to rename these classes and interfaces!
+const HEAD_REG: RegExp = /^(\#{1,6})([^\#\n]+)$/m;
+const CLEAR_HEADING: RegExp = /^(\#*[\s\n]*)/m;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MarkdownHeadingHelperPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'indent-heading',
+			name: 'Indent Heading',
+			hotkeys: [
+				{modifiers: ["Mod", "Shift"], key: ">"}
+			],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				this.indentHeading(editor);
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'hoist-heading',
+			name: 'hoist Heading',
+			hotkeys: [
+				{modifiers: ["Mod", "Shift"], key: "<"}
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.hoistHeading(editor);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: 'sibling-heading',
+			name: 'sibling Heading',
+			hotkeys: [
+				{modifiers: ["Mod", "Shift"], key: "/"}
+			],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.siblingHeading(editor);
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	indentHeading(editor: Editor) {
+		this.insertHeading(editor, 1);
+	}
+
+	hoistHeading(editor: Editor) {
+		this.insertHeading(editor, -1);
+	}
+
+	siblingHeading(editor: Editor) {
+		this.insertHeading(editor, 0, true);
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	insertHeading(editor: Editor, increment: number, isPrevious: boolean = false) {
+		const cursor = editor.getCursor();
+
+		let startHead = cursor.line;
+		const findedHead = this.findPreviousHeadLevel(editor, startHead - (isPrevious ? 1 : 0));
+		if (findedHead !== -1) {
+			const insertHead = Math.min(Math.max(findedHead + increment, 1), 6)
+			const str = editor.getLine(startHead);
+			const heading = CLEAR_HEADING.exec(str);
+			let replaceTo = 0;
+			if (heading) {
+				replaceTo = heading[1].length;
+			}
+
+			editor.replaceRange("#".repeat(insertHead) + ' ', {
+				line: cursor.line,
+				ch: 0
+			}, {
+				line: cursor.line,
+				ch: replaceTo
+			})
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	findPreviousHeadLevel(editor: Editor, startHead: number): number {
+		while (startHead > 0) {
+			let str = editor.getLine(startHead)
+			const result = HEAD_REG.exec(str)
+			if (!!result) {
+				return result[1].length;
+			}
+			startHead--;
+		}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		return -1;
 	}
 }
